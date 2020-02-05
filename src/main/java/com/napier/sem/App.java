316197da7a4b87f1a8ef77a576538dc66b8ee7a6
Mihail Benev <shandura2@gmail.com>
2020-02-05 19:09:31 +0000

    Print all salaries

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,12 +16,31 @@ public class App
 
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
-
+        //Print out salary list
+        a.printSalaries(employees);
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
 
+
         // Disconnect from database
         a.disconnect();
+    }
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
     }
     public void displayEmployee(Employee emp)
     {
@@ -37,6 +56,7 @@ public class App
                             + "Manager: " + emp.manager + "\n");
         }
     }
+
     /**
      * Gets all the current employees and salaries.
      * @return A list of all employees and salaries, or null if there is an error.
